5202b4117860a9f668f29d816b9e1d0a4c6fb572
yashwanth008 <youngtiger.paladugu@gmail.com>
2022-03-18 20:12:47 +0530

    first-commit

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = Color(0xFFFF6347);
+
+BoxShadow kBoxShadow = BoxShadow(
+  color: Colors.grey.withOpacity(0.2),
+  spreadRadius: 2,
+  blurRadius: 8,
+  offset: Offset(0, 0),
+);
